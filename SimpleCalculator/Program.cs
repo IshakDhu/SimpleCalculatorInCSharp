8b433f4977bd84d746957f36ef12c4091930756c
Ishak Dhungyel <ishakdhu@gmail.com>
2023-05-27 11:24:26 -0400

    Added Try Catch Block to Verify User Input!

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,31 +6,40 @@ namespace SimpleCalculator
     {
         public static void Main(string[] args)
         {
-            Console.Write("Please Enter First Number: ");
-            string num1 = Console.ReadLine();
-            Console.Write("Please Enter Second Number: ");
-            string num2 = Console.ReadLine();
-            Console.Write("Please Enter Wanted Operator: ");
-            string stringOperator = Console.ReadLine();
+            try
+            {            Console.Write("Please Enter First Number: ");
+                string num1 = Console.ReadLine();
+                Console.Write("Please Enter Second Number: ");
+                string num2 = Console.ReadLine();
+                Console.Write("Please Enter Wanted Operator: ");
+                string stringOperator = Console.ReadLine();
 
-            double parsedNum1 = double.Parse(num1);
-            double parsedNum2 = double.Parse(num2);
+                double parsedNum1 = double.Parse(num1);
+                double parsedNum2 = double.Parse(num2);
 
-            if (stringOperator.Trim().ToLower() == "+")
-            {
-                Console.WriteLine("Your Answer is " +  (parsedNum1 + parsedNum2));
-            } else if (stringOperator.Trim().ToLower() == "-")
-            {
-                Console.WriteLine("Your Answer is " +  (parsedNum1 - parsedNum2));
-            } else if (stringOperator.Trim().ToLower() == "x")
-            {
-                Console.WriteLine("Your Answer is " + (parsedNum1 * parsedNum2));
-            } else if (stringOperator.Trim().ToLower() == "/")
-            {
-                Console.WriteLine("Your Answer is " + (parsedNum1 / parsedNum2));
+                if (stringOperator.Trim().ToLower() == "+")
+                {
+                    Console.WriteLine("Your Answer is " +  (parsedNum1 + parsedNum2));
+                } else if (stringOperator.Trim().ToLower() == "-")
+                {
+                    Console.WriteLine("Your Answer is " +  (parsedNum1 - parsedNum2));
+                } else if (stringOperator.Trim().ToLower() == "x")
+                {
+                    Console.WriteLine("Your Answer is " + (parsedNum1 * parsedNum2));
+                } else if (stringOperator.Trim().ToLower() == "/")
+                {
+                    Console.WriteLine("Your Answer is " + (parsedNum1 / parsedNum2));
+                }else
+                {
+                    Console.WriteLine("Error: Please Enter A Correct Operator!");
+                }
+
             }
-
-
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please Enter a Correct Number!");
+            }
+            
         }
     }
 
